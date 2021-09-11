1d7f48fd0b3414ff930a5694223419268bb47346
Kitsune-fx <kant.cmd@gmail.com>
2021-09-11 22:46:12 +0700

    [:zap: Create selection]

--- app.js
+++ app.js
@@ -17,7 +17,26 @@ function submit() {
     search = input.value ;
 
     checkInput(search);
-    textResult.innerHTML = search;
-
-    
+    textResult.innerHTML = search;  
 }
+function getData(){
+    const selectProvince = document.querySelector('#province');
+    
+    const apiURL= 'https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces' ;
+
+    fetch(apiURL).then( res =>res.json())
+    .then(data => { 
+        for(let i = 0 ; i<= 77 ; i++){
+
+            var option = document.createElement("option");
+            option.value = i;
+            option.text = data[i]['province'];
+            selectProvince.add(option,null);
+        }
+    })
+}
+function run(){
+    getData();
+}
+
+run();
