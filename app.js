cf18da05f3a279e26baa3743722a83c71764e1d1
Kitsune-fx <kant.cmd@gmail.com>
2021-09-12 14:13:07 +0700

    [:zap:add display function]

--- app.js
+++ app.js
@@ -1,42 +1,44 @@
+const selectProvince = document.querySelector('#province');
+const apiURL= 'https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces' ;
 
-function checkInput(text) {
-    if(text == ''){
-        alert('Please enter the province');
-        return;
+fetch(apiURL).then( res =>res.json())
+.then(data => { 
+    for(let i = 0 ; i<= 77 ; i++){
+        var option = document.createElement("option");
+        option.value = i;
+        option.text = data[i]['province'];
+        selectProvince.add(option,null);
     }
-    else{
-    console.log(search);
-    input.value = '';
-    }
-}
+})
 
 
 function submit() {
-    const input = document.querySelector('#input');
-    const textResult =  document.querySelector('#textResult');
-    search = input.value ;
-
-    checkInput(search);
-    textResult.innerHTML = search;  
-}
-function getData(){
-    const selectProvince = document.querySelector('#province');
-    
-    const apiURL= 'https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces' ;
-
-    fetch(apiURL).then( res =>res.json())
-    .then(data => { 
-        for(let i = 0 ; i<= 77 ; i++){
-
-            var option = document.createElement("option");
-            option.value = i;
-            option.text = data[i]['province'];
-            selectProvince.add(option,null);
-        }
-    })
-}
-function run(){
-    getData();
+    const select = document.querySelector('#province');
+    search = select.value ;
+    display(search);
 }
 
-run();
+function display(index){
+    let text = 'Please enter the province';
+    const textResult = document.querySelector('#textResult');
+    fetch(apiURL).then(response => response.json())
+    .then(result => {
+        text = `<h2 class='text-header'> ${result[index]['province']} </h2> 
+        <div class='text-case'>
+            New case : ${result[index]['new_case']}      
+            Total case : ${result[index]['total_case']}
+        </div>
+        <div class='text-death'>
+            New death: ${result[index]['new_death']}      
+            Total death: ${result[index]['total_death']}
+        </div>
+        <div class = 'text-date'>
+            Update date : ${result[index]['update_date']}
+        </div>
+        `;
+
+        textResult.innerHTML = text;
+    } )
+}
+
+
